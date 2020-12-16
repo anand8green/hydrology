e1dab994b99ab504686bbead12e0bf4ea361f053
Anand Kumar <anandkumar@Anands-iMac.local>
2020-12-16 14:41:09 +0000

    update newsitem

--- pages/Component/Home/News/NewsItem.js
+++ pages/Component/Home/News/NewsItem.js
@@ -1,10 +1,23 @@
 
+import { useMediaQuery } from 'react-responsive'
+
 export default function NewsItem({ category, title, date, image, index, slug }) {
+
+    const isMobile = useMediaQuery({ query: `(max-width: 800px)` })
+
+    console.log(isMobile);
 
     return (
         <a href={`news/${slug}`} key={index} >
             <div className="NewsItem"
                 key={index}
+                style={{
+                    padding: 0,
+                    width: index === 0 && !isMobile ? 550 : null,
+                    // width: index === 0 && isMobile ? '100%' : null,
+                    // backgroundColor: isMobile ? "red" : "green",
+
+                }}
             >
                 <img className="pic" src={image}>
                 </img>
