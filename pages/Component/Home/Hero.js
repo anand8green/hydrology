5266fe8ba31413d98c1cc626a79f4daa08eec15f
Anand Kumar <anandkumar@Anands-iMac.local>
2020-12-31 12:03:03 +0000

    update

--- pages/Component/Home/Hero.js
+++ pages/Component/Home/Hero.js
@@ -1,26 +1,37 @@
 import NavBar from "../NavBar";
 import BubbleNew from "./BubbleNew";
 import Link from 'next/link'
+import { useState } from "react";
+import { useRouter } from 'next/router'
 
 export default function Hero({ title, subtitle }) {
+
+    const [email, setEmail] = useState("")
+    const router = useRouter()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        router.push('/quote')
+    }
+
     return (
-
         <div className="Hero">
-
             <NavBar />
-
             <div className="Hero__Content">
                 <div className="Hero__Text">
                     <h1>{title}</h1>
                     <p>{subtitle}</p>
                 </div>
+                <form action="" onSubmit={handleSubmit}>
+                    <div className="Hero__Form">
+                        <input type="email" required placeholder="E-mail address"
+                            value={email} onChange={(e) => { setEmail(e.target.value) }}
+                        />
+                        <button type="submit" >Get a Quote</button>
 
-                <div className="Hero__Form">
-                    <input type="text" placeholder="E-mail address" />
-                    <Link href="/quote">
-                        <button>Get a Quote</button>
-                    </Link>
-                </div>
+                    </div>
+                </form>
+
             </div>
 
             {/* <BubbleBg /> */}
--- pages/contact.js
+++ pages/contact.js
@@ -32,8 +32,8 @@ export default function About({ heading, subheading, body }) {
                         className="contactInfo"
                     />
 
-                    <div className="map" style={{ borderRadius: 20, overflow: 'hidden' }}>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2290.0390427386606!2d-1.625569184099623!3d54.972414480351915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x54b89016fafd9d53!2sGreen!5e0!3m2!1sen!2suk!4v1559568093914!5m2!1sen!2suk" width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style={{ height: '100%' }}></iframe>
+                    <div className="map" >
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2290.0390427386606!2d-1.625569184099623!3d54.972414480351915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x54b89016fafd9d53!2sGreen!5e0!3m2!1sen!2suk!4v1559568093914!5m2!1sen!2suk" width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" ></iframe>
 
                     </div>
                 </div>
